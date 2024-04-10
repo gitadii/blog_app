32e724db6026658e7fca767cee6beb55a72561a0
gitadi <aduvishwakarma123@gmail.com>
2024-04-10 10:17:13 +0530

    userCase interface and signUp interface

---
+++ lib/features/auth/domain/usecases/user_sign_up.dart
@@ -0,0 +1,27 @@
+import 'package:blog_app/core/error/failure.dart';
+import 'package:blog_app/core/usecases/usecase_interf.dart';
+import 'package:blog_app/features/auth/domain/repository/auth_repository.dart';
+import 'package:fpdart/fpdart.dart';
+
+class UserSignUp implements UseCase<String, UserSignUpParams> {
+  final AuthRepository authRepository;
+  UserSignUp({required this.authRepository});
+
+  @override
+  Future<Either<Failure, String>> call(UserSignUpParams params) async {
+    return await authRepository.signUpWithEmail(
+      name: params.name,
+      email: params.email,
+      password: params.password,
+    );
+  }
+}
+
+class UserSignUpParams {
+  final String name;
+  final String email;
+  final String password;
+
+  UserSignUpParams(
+      {required this.name, required this.email, required this.password});
+}
